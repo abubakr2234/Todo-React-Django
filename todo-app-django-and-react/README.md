0a282176a5b7fcb80fe55f372764b862a58e59ce
abubakr2234 <68422152+abubakr2234@users.noreply.github.com>
2020-12-01 10:51:50 +0200

    Add files via upload

---
+++ todo-app-django-and-react/README.md
@@ -0,0 +1,2 @@
+# todo-app-django-and-react
+using django as the backend and react as the front end
---
+++ todo-app-django-and-react/todo/reactapp/src/App.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+  return (
+    <div className="App">
+
+        <div className="center-column">
+
+       
+          <div className="item-row">
+            <span>Todo1</span>
+          </div>
+
+          <div className="item-row">
+            <span>Todo2</span>
+          </div>
+
+          <div className="item-row">
+            <span>Todo3</span>
+          </div>
+
+          <div className="item-row">
+            <span>Todo4</span>
+          </div>
+
+          <div className="item-row">
+            <span>Todo5</span>
+          </div>
+
+          <div className="item-row">
+            <span>Todo6 </span>
+          </div>
+
+        </div>
+     
+    </div>
+  );
+}
+
+export default App;
